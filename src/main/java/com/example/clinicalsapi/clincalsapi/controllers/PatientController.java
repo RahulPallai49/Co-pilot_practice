ef008d8e3ced796ed91e851774d875e3dcfe0c2b
RahulPallai49 <145913264+RahulPallai49@users.noreply.github.com>
2024-07-29 10:24:51 +0530

    Update PatientController.java

--- src/main/java/com/example/clinicalsapi/clincalsapi/controllers/PatientController.java
+++ src/main/java/com/example/clinicalsapi/clincalsapi/controllers/PatientController.java
@@ -3,7 +3,11 @@ package com.example.clinicalsapi.clincalsapi.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import com.example.clinicalsapi.clincalsapi.exceptions.PatientNotFoundException;
 import com.example.clinicalsapi.clincalsapi.models.Patient;
 import com.example.clinicalsapi.clincalsapi.repo.PatientRepository;
 
@@ -28,16 +32,22 @@ public class PatientController {
         return patients;
     }
 
-    @GetMapping("/{id}")
-    public Optional<Patient> getPatientById(@PathVariable Long id) {
-        logger.info("Fetching patient with id {}", id);
-        Optional<Patient> patient = patientRepository.findById(id);
-        if (patient.isPresent()) {
-            logger.info("Patient found: {}", patient.get());
-        } else {
-            logger.warn("Patient with id {} not found", id);
+   @GetMapping("/{id}")
+    public ResponseEntity<?> getPatientById(@PathVariable Long id) {
+        try {
+            logger.info("Fetching patient with id {}", id);
+            Optional<Patient> patient = patientRepository.findById(id);
+            if (patient.isPresent()) {
+                logger.info("Patient found: {}", patient.get());
+                return ResponseEntity.ok(patient.get());
+            } else {
+                logger.warn("Patient with id {} not found", id);
+                throw new PatientNotFoundException("Patient not found with id: " + id);
+            }
+        } catch (PatientNotFoundException e) {
+            logger.error("Error: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
-        return patient;
     }
 
     @PostMapping
